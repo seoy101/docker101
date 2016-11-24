fae4e5755754e1ca6bc4969c94abe966e1dd1b2e
ichthys1124 <ichthys1124@gmail.com>
2016-11-24 11:34:54 +0900

    Update install_docker_centos.sh

--- install_docker_centos.sh
+++ install_docker_centos.sh
@@ -3,15 +3,10 @@
 cat > /etc/yum.repos.d/docker.repo << '__EOF__'
 
 [dockerrepo]
-
 name=Docker Repository
-
 baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-
 enabled=1
-
 gpgcheck=1
-
 gpgkey=https://yum.dockerproject.org/gpg
 
 __EOF__
@@ -21,9 +16,7 @@ mkdir -p /etc/systemd/system/docker.service.d
 cat > /etc/systemd/system/docker.service.d/override.conf << '__EOF__'
 
 [Service]
-
 ExecStart=
-
 ExecStart=/usr/bin/docker daemon --storage-driver=overlay -H fd://
 
 __EOF__
