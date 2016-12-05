19f391780f078771b71ac587a774bc05ea648ede
seoy101 <fighton.yj@gmail.com>
2016-12-05 15:04:28 +0900

    Create install_gnome(centos_minimal).sh

---
+++ install_gnome(centos_minimal).sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+
+yum groupinstall -y "X Window System"
+
+yum groupinstall -y "Internet Browser"
+
+yum install -y gnome-classic-session gnome-terminal nautilus-open-terminal control-center liberation-mono-fonts
+
+systemctl stop firewalld
+
+systemctl disable firewalld
+
+ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
